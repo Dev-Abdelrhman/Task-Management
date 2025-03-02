3fd704b04a1609b14f381be21823e2da52cd6463
Omar-YasserVV <138281006+Omar-YasserVV@users.noreply.github.com>
2025-03-02 10:30:17 +0200

    O-Auth

---
+++ Backend/controllers/O-AuthControllers.js
@@ -0,0 +1,140 @@
+import express from "express";
+import bcrypt from "bcrypt";
+import passport from "passport";
+import { Strategy } from "passport-local";
+import GoogleStrategy from "passport-google-oauth2";
+import env from "dotenv";
+import jwt from "jsonwebtoken";
+
+
+const app = express();
+const saltRounds = 10;
+app.use(express.static("public"));
+env.config();
+
+
+const protect =  async (req, res) => {
+  //It checks if the user session exists 
+    if (req.isAuthenticated()) {
+      try {
+        //It checks if the user exists in the database and give access to the user
+        //check if it admin or user
+      } catch (err) {
+        console.log(err);
+      }
+    } else {
+      res.redirect("/login");
+    }
+  };
+  
+  app.get(
+    "/auth/google",
+    passport.authenticate("google", {
+      scope: ["profile", "email"],
+    })
+  );
+
+  app.post(
+    "/login",
+    passport.authenticate("local", {
+      successRedirect: "/", // redirect to the secure profile section
+      failureRedirect: "/login",
+    })
+  );
+
+  app.post("/register", async (req, res) => {
+    const email = "";// email from req remove the dubious quotes it is just a placeholder
+    const password = ""; // password from req remove the dubious quotes it is just a placeholder
+  
+    try {
+      const checkResult = await db.query("SELECT * FROM users WHERE email = $1", [
+        email,
+      ]);
+  
+      if (checkResult.rows.length > 0) {
+        req.redirect("/login");
+      } else {
+        bcrypt.hash(password, saltRounds, async (err, hash) => {
+          if (err) {
+            console.error("Error hashing password:", err);
+          } else {
+            const result = await db.query(
+              "INSERT INTO users (email, password) VALUES ($1, $2) RETURNING *",
+              [email, hash]
+            );
+            const user = result.rows[0];
+            req.login(user, (err) => {
+            //   console.log("success");
+              res.redirect("/secrets");
+            });
+          }
+        });
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  });
+
+  // Passport local strategy
+  passport.use(
+    "local",
+    new Strategy(async function verify(username, password, cb) {
+        // 
+      try {
+        const result = await db.query("SELECT * FROM users WHERE email = $1 ", [
+          username,
+        ]);
+        if (result.rows.length > 0) {
+          const user = result.rows[0];
+          const storedHashedPassword = user.password;
+          bcrypt.compare(password, storedHashedPassword, (err, valid) => {
+            if (err) {
+              console.error("Error comparing passwords:", err);
+              return cb(err);
+            } else {
+              if (valid) {
+                return cb(null, user);
+              } else {
+                return cb(null, false);
+              }
+            }
+          });
+        } else {
+          return cb("User not found");
+        }
+      } catch (err) {
+        console.log(err);
+      }
+    })
+  );
+  
+  passport.use(
+    "google",
+    new GoogleStrategy(
+      {
+        clientID: process.env.GOOGLE_CLIENT_ID,
+        clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+        callbackURL: "http://localhost:3000/auth/google/secrets",
+        userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo",
+      },
+      async (accessToken, refreshToken, profile, cb) => {
+        try {
+          console.log(profile);
+          const result = await db.query("SELECT * FROM users WHERE email = $1", [
+            profile.email,
+          ]);
+          if (result.rows.length === 0) {
+            const newUser = await db.query(
+              "INSERT INTO users (email, password) VALUES ($1, $2)",
+              [profile.email, "google"] // password is just a placeholder
+            );
+            return cb(null, newUser.rows[0]);
+          } else {
+            return cb(null, result.rows[0]);
+          }
+        } catch (err) {
+          return cb(err);
+        }
+      }
+    )
+  );
