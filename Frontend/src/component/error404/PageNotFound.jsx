8516b11cdd623cac87b4d0b780c830950fe60f30
Omar-YasserVV <138281006+Omar-YasserVV@users.noreply.github.com>
2025-03-03 09:56:37 +0200

    .

---
+++ Frontend/src/component/error404/PageNotFound.jsx
@@ -0,0 +1,9 @@
+function PageNotFound() {
+    return (
+      <div className="alert alert-danger my-5 py-3 w-50 mx-auto">
+        Error 404: Page Not Found
+      </div>
+    );
+  }
+  
+  export default PageNotFound;
