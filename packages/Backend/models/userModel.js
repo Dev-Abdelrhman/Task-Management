717f72ee92bcf8c671fc90f498823a451cf09b89
Dev-Abdelrhman <abdelrhmanfawzy38@gmail.com>
2025-03-12 11:57:08 +0200

    `Update userModel.js: change passwordConfirm to passwordConfirmation in pre-save hook`

--- packages/Backend/models/userModel.js
+++ packages/Backend/models/userModel.js
@@ -54,7 +54,7 @@ const userSchema = new mongoose.Schema({
 userSchema.pre("save", async function (next) {
   if (!this.isModified("password")) return next();
   this.password = await bcrypt.hash(this.password, 12);
-  this.passwordConfirm = undefined;
+  this.passwordConfirmation = undefined;
   next();
 });
 
