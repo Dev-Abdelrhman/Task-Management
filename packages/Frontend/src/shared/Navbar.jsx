4b3d2339115297b814b40903a5fc01faa3c0da1c
Omar Yasser Shawky <bakryasser744@gmail.com>
2025-03-31 10:45:31 +0200

    Update Navbar and Sidebar components; enhance user experience with improved styling and functionality, including user image display and new navigation links

--- packages/Frontend/src/shared/Navbar.jsx
+++ packages/Frontend/src/shared/Navbar.jsx
@@ -1,11 +1,21 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useAuth } from "../hooks/useAuth";
-import { useNavigate } from "react-router-dom";
 import { toast } from "react-toastify";
 import { Button } from "@mui/material";
+import { useAuthStore } from "../stores/authStore";
+import Box from "@mui/material/Box";
+import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+import Menu from "@mui/material/Menu";
+
+import Avatar from "@mui/material/Avatar";
+import Tooltip from "@mui/material/Tooltip";
+import MenuItem from "@mui/material/MenuItem";
 
 function Navbar() {
   const { signOut, isLoading } = useAuth();
+  const { user } = useAuthStore();
+  const [anchorElUser, setAnchorElUser] = React.useState(null);
 
   const handleLogout = async () => {
     try {
@@ -14,22 +24,58 @@ function Navbar() {
       toast.error(`Logout failed: ${error.message}`);
     }
   };
+  useEffect(() => {
+    console.log(user.image);
+  }, []);
 
+  const handleOpenUserMenu = (event) => {
+    setAnchorElUser(event.currentTarget);
+  };
+
+  const hostGoogleImage = (url) => {
+    return `https://images.weserv.nl/?url=${encodeURIComponent(
+      url
+    )}&w=200&h=200`;
+  };
+
+  const handleCloseUserMenu = () => {
+    setAnchorElUser(null);
+  };
   return (
-    <nav className="flex justify-between items-center bg-white shadow-sm border-b border-gray-200 px-6 py-4 fixed top-0 left-64 right-0 z-10">
-      <h4 className="text-xl font-semibold text-gray-800">Explore</h4>
-      
-      <div className="flex items-center gap-4">
-        <Button
-          variant="contained"
-          onClick={handleLogout}
-          disabled={isLoading}
-          className="!bg-red-600 hover:!bg-red-700 !text-white !px-4 !py-2 !rounded-lx !transition-colors !shadow-sm"
-        >
-          {isLoading ? "Logging out..." : "Logout"}
-        </Button>
-      </div>
-    </nav>
+    <>
+      <nav className="flex justify-between items-center bg-white px-6 py-4 fixed top-0 left-64 right-0 z-10">
+        <h4 className="text-2xl font-medium !text-[##53577A]-800">
+          Hi, {user.username}
+        </h4>
+        <Box sx={{ flexGrow: 0 }}>
+          <Tooltip title="Open settings">
+            <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+              <Avatar src={hostGoogleImage(user.image)} />
+            </IconButton>
+          </Tooltip>
+          <Menu
+            sx={{ mt: "45px" }}
+            id="menu-appbar"
+            anchorEl={anchorElUser}
+            anchorOrigin={{
+              vertical: "top",
+              horizontal: "right",
+            }}
+            keepMounted
+            transformOrigin={{
+              vertical: "top",
+              horizontal: "right",
+            }}
+            open={Boolean(anchorElUser)}
+            onClose={handleCloseUserMenu}
+          >
+            <MenuItem key="logout" onClick={handleLogout} disabled={isLoading}>
+              <Typography sx={{ textAlign: "center" }}>Logout</Typography>
+            </MenuItem>
+          </Menu>
+        </Box>
+      </nav>
+    </>
   );
 }
 
