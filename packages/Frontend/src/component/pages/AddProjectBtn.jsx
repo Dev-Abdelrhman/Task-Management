35ad6150a62e3278b17051850594a56306bf73cb
Mostafa Mohamed <99538301+MostafaMohamedgamil@users.noreply.github.com>
2025-04-13 21:11:01 +0200

    handle the desgin of navbar of project page

--- packages/Frontend/src/component/pages/AddProjectBtn.jsx
+++ packages/Frontend/src/component/pages/AddProjectBtn.jsx
@@ -47,7 +47,7 @@ function AddProjectBtn() {
                     className="bg-white !rounded-xl shadow-md p-6 w-full max-w-md"
                     onClick={(e) => e.stopPropagation()}>
                     <div className="flex justify-between text-center items-center mb-4">
-                        <h3 className="text-xl font-semibold text-gray-900">Create New Product</h3>
+                        <h3 className="text-xl font-semibold text-gray-900">Create New Project</h3>
                         <button
                             onClick={() => setShowModal(false)}
                             className="text-gray-400 hover:text-gray-900">
