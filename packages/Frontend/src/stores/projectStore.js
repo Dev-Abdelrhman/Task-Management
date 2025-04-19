6fd63ab41a787e13e816877c420d9a1e4ba23990
Omar Yasser Shawky <bakryasser744@gmail.com>
2025-04-19 02:47:50 +0200

    hotfix and update fix refresh token error Update CODEOWNERS, change page title to 'Taskord', simplify auth error handling, and refactor image hosting in Dashboard and Google Sign-in components. Remove unused project store methods.

--- packages/Frontend/src/stores/projectStore.js
+++ packages/Frontend/src/stores/projectStore.js
@@ -1,15 +1,5 @@
 import { create } from "zustand";
 
 export const useProjecthStore = create((set) => ({
-  Projects: null,
 
-  setProject: (user) => {
-    localStorage.setItem("user", JSON.stringify(user));
-    set({ user });
-  },
-
-  logout: () => {
-    set({ user: null });
-    localStorage.removeItem("user");
-  },
 }));
