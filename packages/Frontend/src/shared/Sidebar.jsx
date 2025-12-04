c5e4c69e5635049fe947307bf3c0b5636ecfec9b
Omar-YasserVV <bakryasser744@gmail.com>
2025-12-04 06:50:07 +0200

    fix back drop for the light mode for the sidebar icon

--- packages/Frontend/src/shared/Sidebar.jsx
+++ packages/Frontend/src/shared/Sidebar.jsx
@@ -23,7 +23,7 @@ function Sidebar() {
   return (
     <>
       <button
-        className="md:hidden fixed top-5 !bg-black/70 backdrop-blur-sm left-8 z-50 border-[1px] rounded-full text-[#8E92BC] p-3"
+        className="md:hidden fixed top-5 dark:!bg-black/70 backdrop-blur-sm left-8 z-50 border-[1px] rounded-full text-[#8E92BC] p-3"
         onClick={toggleSidebar}
       >
         <Menu />
